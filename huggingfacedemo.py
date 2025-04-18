cc1755b869d88db64a3a36bb2136a2878cffbf5a
송석리(Song Sukree) <greatsong21@gmail.com>
2025-04-18 12:55:49 +0900

    Create huggingfacedemo.py

---
+++ huggingfacedemo.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from transformers import pipeline
+
+st.title('허깅페이스 데모')
+text = st.text_input("분석 위한 텍스트")
+model = pipeline('sentiment-analysis')
+
+if text :
+  result = model(text)
+  st.write('감정 : ", result[0]['label'])
+  st.write('점수 : ", result[0]['score'])
