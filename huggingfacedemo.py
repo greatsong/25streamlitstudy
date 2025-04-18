b5f6e68a9f5c86a6ea0e94e8e99e07ba80ef7290
송석리(Song Sukree) <greatsong21@gmail.com>
2025-04-18 13:01:49 +0900

    Update huggingfacedemo.py

--- huggingfacedemo.py
+++ huggingfacedemo.py
@@ -2,8 +2,19 @@ import streamlit as st
 from transformers import pipeline
 
 st.title('허깅페이스 데모')
+
+@st.cache_resource
+def load_model():
+    return pipeline('sentiment-analysis')
+
+model = load_model()
+
 text = st.text_input("분석 위한 텍스트")
-model = pipeline('sentiment-analysis')
+
+if text:
+    result = model(text)
+    st.write(result)
+
 
 if text :
   result = model(text)
